c3677b66f3b0db454fcf887f1a15b68eaee60b0f
werasaimon <nicemankit@mail.ru>
2017-05-19 04:38:01 +0300

    Engine

---
+++ engine/engine.h
@@ -0,0 +1,13 @@
+
+
+#include "physics-engine/physics.h"
+#include "physics-engine/realphysics.h"
+
+#include "UI-engine/engine.h"
+
+#include "lua-interpreter/loadlibarylua.h"
+#include "lua-interpreter/loadlibaryluavalue.h"
+#include "lua-interpreter/loadlibaryluaopengl.h"
+#include "lua-interpreter/loadlibaryluashader.h"
+#include "lua-interpreter/loadlibaryluaphysicsengine.h"
+#include "lua-interpreter/loadlibaryluauiengine.h"
