b8c00a8914340645bf36603291d8cdf3284e1ac4
werasaimon <nicemankit@gmail.com>
2017-05-22 01:36:59 +0300

    main

---
+++ src/main.cpp
@@ -0,0 +1,153 @@
+
+#include <iostream>
+
+#include "examples/GLViewer.h"
+#include "examples/UnitScene.h"
+#include "examples/UnitSceneDynamics.h"
+
+using namespace std;
+using namespace opengl_utility;
+
+
+int Width  = 600;
+int Height = 400;
+
+int   world_size = 20;
+
+float mouse_x , mouse_y ;
+int   mouse_b = 0;
+
+float timeStep = 1.0f / 60.0f;
+
+
+
+UnitScene       *Scene;
+Viewer          *viewer;
+
+
+
+
+
+// Declarations
+void simulate();
+void display();
+void reshape(int width, int height);
+void mouseButton(int button, int state, int x, int y);
+void mouseMotion(int x, int y);
+void keyboard(unsigned char key , int x  , int y);
+void special(int key , int x  , int y);
+void init();
+void Timer(int t);
+
+// Main function
+int main(int argc, char** argv)
+{
+
+
+	/**/
+		// Initialize GLUT
+		viewer = new Viewer();
+		viewer->init( argc , argv , "Test" , opengl_utility::Vector2(Width,Height) , opengl_utility::Vector2(0,0) , false );
+		viewer->initilisation();
+
+
+
+		Scene = new  UnitSceneDynamics( viewer );
+
+		// Create the scene
+		if(!Scene->Init())
+		{
+			cout<< "error initialization" <<endl;
+		}
+
+		// Create and initialize the Viewer
+		bool initOK = true;
+		if (!initOK) return 1;
+
+		init();
+
+		// Glut Idle function that is continuously called
+		glutIdleFunc(simulate);
+		glutDisplayFunc(display);
+		glutReshapeFunc(reshape);
+		glutKeyboardFunc(keyboard);
+		glutSpecialFunc(special);
+		glutMouseFunc(mouseButton);
+		glutMotionFunc(mouseMotion);
+
+		//glutTimerFunc(0, Timer, (int) 100.0f / 60.0f);
+
+		// Glut main looop
+		glutMainLoop();
+
+		//delete scene;
+		Scene->Destroy();
+
+		delete Scene;
+		delete viewer;
+
+		/**/
+		return 0;
+}
+
+// Simulate function
+void simulate()
+{
+    // Display the scene
+    display();
+}
+
+// Initialization
+void init()
+{
+    // Define the background color (black)
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+
+}
+
+// Reshape function
+void reshape(int width, int height)
+{
+     viewer->reshape(width, height);
+}
+
+
+
+void keyboard(unsigned char key , int x  , int y)
+{
+     viewer->keyboard(key , x , y);
+     Scene->keyboard(key);
+}
+
+void special(int key , int x  , int y)
+{
+     viewer->special(key, x, y);
+}
+
+// Called when a mouse button event occurs
+void mouseButton(int button, int state, int x, int y)
+{
+	viewer->mouseButtonEvent(button, state, x , y);
+}
+
+// Called when a mouse motion event occurs
+void mouseMotion(int x, int y)
+{
+	viewer->motion(x, y);
+}
+
+// Display the scene
+void display()
+{
+	viewer->update();
+	Scene->Render(timeStep);
+
+    glutSwapBuffers();
+}
+
+
+void Timer(int t)
+{
+	display();
+	glutTimerFunc(t , Timer, (int) 1.0f / 60.0f);
+}
