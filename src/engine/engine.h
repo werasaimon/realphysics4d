4d3b90cdb355849a95f3c280ee9498ea4a71f5ed
werasaimon <nicemankit@mail.ru>
2017-06-03 05:07:37 +0300

    engine

---
+++ src/engine/engine.h
@@ -0,0 +1,18 @@
+/*
+ * engine.h
+ *
+ *  Created on: 2 июн. 2017 г.
+ *      Author: werqa
+ */
+
+#ifndef SRC_ENGINE_ENGINE_H_
+#define SRC_ENGINE_ENGINE_H_
+
+
+#include "physics-engine/physics.h"
+#include "physics-engine/realphysics.h"
+
+#include "UI-engine/ui_engine.h"
+
+
+#endif /* SRC_ENGINE_ENGINE_H_ */
