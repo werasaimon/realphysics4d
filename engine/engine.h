c744a6de4b42fef29df20a30332304ddf974926e
werasaimon <nicemankit@mail.ru>
2017-06-04 02:16:32 +0300

    engine

--- engine/engine.h
+++ engine/engine.h
@@ -1,11 +1,4 @@
-#include "physics-engine/physics.h"
-#include "physics-engine/realphysics.h"
+#ifndef ENGINE_H
+#define ENGINE_H
 
-#include "UI-engine/engine.h"
-
-#include "lua-interpreter/loadlibarylua.h"
-#include "lua-interpreter/loadlibaryluavalue.h"
-#include "lua-interpreter/loadlibaryluaopengl.h"
-#include "lua-interpreter/loadlibaryluashader.h"
-#include "lua-interpreter/loadlibaryluaphysicsengine.h"
-#include "lua-interpreter/loadlibaryluauiengine.h"
+#endif // ENGINE_H
